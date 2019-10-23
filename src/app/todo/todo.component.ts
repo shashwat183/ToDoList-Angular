44457d87da4762e4b234b494b6deac8130f6ff44
Shashwat pragya <shashwat183@gmail.com>
2019-10-23 22:11:16 +0100

    fix

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -11,7 +11,7 @@ export class TodoComponent implements OnInit {
   constructor() {
     this.todoTasks.push('Learn Angular');
     this.todoTasks.push('Go to Gym');
-    this.todoTasks.push('Clean Kitchen')
+    this.todoTasks.push('Clean Kitchen');
    }
 
   ngOnInit() {
