7c1f53a810eef12111a1879edd8255c859b54040
shashwat pragya <shashwat183@gmail.com>
2020-03-08 06:29:13 +0000

    Add home page with login support
    Added home page with proper shared header and footer. Enabled
    login support with google and facebook Oauth apis. The logged in
    data is not used currently but will be used later on to customize
    and personalize the tasks view for the user.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {Router} from '@angular/router';
+import { AuthService, FacebookLoginProvider, GoogleLoginProvider, SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  invalidData: boolean;
+  user: SocialUser;
+  loggedIn: boolean;
+  loginForm = new FormGroup({
+    emailAddress: new FormControl(null,
+      [Validators.email,
+        Validators.required]
+      ),
+    password: new FormControl(null,
+      Validators.required
+      )
+  });
+
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+      this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      console.log(this.user);
+    });
+  }
+
+  onSubmit(buttonType: string) {
+    if (this.loginForm.invalid) {
+      this.invalidData = true;
+      return;
+    }
+    if (buttonType === 'google') {
+      this.signInWithGoogle();
+    }
+    if (buttonType === 'facebook') {
+      this.signInWithFb();
+    }
+    this.router.navigate(['/tasks']);
+  }
+
+  signInWithGoogle() {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFb() {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
   }
 
 }
